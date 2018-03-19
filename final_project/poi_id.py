081bc7c0834ca9300c62ef797a58f8b01009f9a2
Carina Shen <carinashen@Carinas-MacBook-Pro.local>
2018-03-18 17:25:29 -0700

    just for backup

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -9,15 +9,25 @@ from tester import dump_classifier_and_data
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
-### The first feature must      sbe "poi".      
-features_list = ['poi','salary'] # You will need to use more features
+### The first feature must      sbe "poi".
+features_list = ['poi','long term incentive','deferral income','deferral payments','restricted stock deferred'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop('Total','THE TRAVEL AGENCY IN THE PARK')
 ### Task 3: Create new feature(s)
+def new_features(total payments,total stock value):
+    new_list=[]
+    for i in data_dict:
+        if data_dict[i][total payments]=="NaN"or data_dict[i][total payments]==0:
+            new_list.append(0)
+        else
+            new_list.append(data_dict[i][total stock value]/data_dict[i][total payments])
+    return new_list
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -33,19 +43,25 @@ labels, features = targetFeatureSplit(data)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+clfNB = GaussianNB()
+from sklearn.svm import svm
+clfSV = SVC(kernel = "linear")
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+clfNB.fit(features_test,labels_train)
+pred=clfNB.predict(features_test)
+clfSV.fit(features_train,labels_train)
+pred=clfSV.predict(features_test)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
